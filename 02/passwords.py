fc52dbe198101c6d0957ab02f67556c7a0ba0f2e
paullickman <56133210+paullickman@users.noreply.github.com>
2020-12-02 09:04:51 +0000

    Add files via upload

---
+++ 02/passwords.py
@@ -0,0 +1,31 @@
+import re
+import collections
+
+Password = collections.namedtuple('Password', 'lowest highest letter password')
+
+def valid(p):
+    n = len(re.findall(p.letter, p.password))
+    return n >= p.lowest and n <= p.highest
+
+def valid2(p):
+    pos1 = p.password[p.lowest-1] == p.letter
+    pos2 = p.password[p.highest-1] == p.letter
+    return (pos1 and not(pos2)) or (not(pos1) and pos2)
+
+class Passwords():
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            strip = map(lambda x: re.split('-| |: ', x.strip()), f.readlines())
+        self.passwords = list(map(lambda x: Password(int(x[0]), int(x[1]), x[2], x[3]), strip))
+
+    def numValid(self, v=valid):
+        return len(list(filter(v, self.passwords)))
+
+p = Passwords('test.txt')
+assert(p.numValid() == 2)
+assert(p.numValid(valid2) == 1)
+
+p = Passwords('input.txt')
+print(p.numValid())
+print(p.numValid(valid2))
